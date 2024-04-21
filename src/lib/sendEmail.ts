1e55f11a8f35ff5e5475fccf5721e63be81c6404
Matheus Aurélio <matheusaurelio2004@gmail.com>
2024-04-21 17:09:50 -0300

    :pencil: format body email with infos user

--- src/lib/sendEmail.ts
+++ src/lib/sendEmail.ts
@@ -18,7 +18,23 @@ export async function sendEmail(data: ContactData) {
     from: data.email,
     to: process.env.SMTP_EMAIL,
     subject: data.subject,
-    html: data.textarea
+    html: `
+    <body>
+      <h1 class="text-2xl font-bold mb-4">Assunto do E-mail</h1>
+      <p class="mb-6">Olá, você recebeu uma mensagem de:</p>
+      <p><strong>Nome:</strong><br /> ${data.name}</p>
+      <hr />
+      <p><strong>Email:</strong><br /> ${data.email}</p>
+      <hr />
+      <p><strong>Telefone:</strong><br /> ${data.telephone}</p>
+      <hr />
+      <p><strong>Assunto:</strong><br /> ${data.subject}</p>
+      <hr />
+      <p><strong>Mensagem:</strong><br /> ${data.textarea}</p>
+      <hr />
+      <h3>Muito obrigado pelo contato, vou tentar respondê-lo o mais rápido possível!</h3>
+    </body>
+    `
   }
 
   await transporter.sendMail(mailOptions)
